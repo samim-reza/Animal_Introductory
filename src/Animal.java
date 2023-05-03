30acdcd21b4155364b31e5650125b38c56c35e04
samim101 <samimreza2111@gmail.com>
2023-05-04 03:17:47 +0600

    JavaProject

---
+++ src/Animal.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+
+abstract class Animal extends JFrame{
+    private String name="null";
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public String getName(){
+        return this.name;
+    }
+    abstract void walk();
+    abstract void eat();
+    abstract void sleep();
+}
+class Ant extends Animal{
+    private Container c;
+    private JLabel imgLabel;
+    private ImageIcon img;
+    private JPanel jp;
+    Ant(){
+        init();
+    }
+    public void init(){
+        c = getContentPane();
+        c.setLayout(null);
+        setLayout(new GridLayout(2, 2));
+        setTitle("My Frame");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setVisible(true);
+        img = new ImageIcon("src/Animal/Ant/Ant.jpg");
+        imgLabel = new JLabel(img);
+        imgLabel.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
+        c.add(imgLabel);
+    }
+    @Override
+    public void walk() {
+        img = new ImageIcon("src/Animal/Ant/AntWalk.gif");
+        imgLabel.setIcon(img);
+    
+    
+    }
+  
+    @Override
+    void eat() {
+
+    }
+
+    @Override
+    void sleep() {
+
+    }
+
+}
---
+++ src/Pages.java
@@ -0,0 +1,125 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Arrays;
+
+public class Pages extends JFrame implements MouseListener, ActionListener {
+    private Container c;
+    private JPanel[] subPanels = new JPanel[28];
+    private JPanel[] mainPanel = new JPanel[7];
+    private JPanel buttonPanel = new JPanel();
+    private JLabel[] imgLabel=new JLabel[28];
+    private ImageIcon[] img=new ImageIcon[28];
+    private JButton prevButton;
+    private JButton nextButton;
+    private int subPanelIndex = 0, currentPanelIndex = 0;
+    private String[] animalName={"src/Animal/Ant/Ant.jpg","src/Animal/Bear/Bear.jpg","src/Animal/Cat/Cat.png","src/Animal/Dog/Dog.jpg","src/Animal/Elephant/Elephant.jpg","src/Animal/Fox/Fox.jpg","src/Animal/Giraffe/Giraffe.jpg","src/Animal/Horse/Horse.jpg","src/Animal/Ibis/Ibis.jpg","src/Animal/Jaguar/Jaguar.jpg","src/Animal/Kangaroo/Kangaroo.jpg","src/Animal/Lion/Lion.jpg","src/Animal/Monkey/Monkey.jpg","src/Animal/Needlefish/Needlefish.png","src/Animal/Ostrich/Ostrich.jpeg","src/Animal/Panda/Panda.jpg","src/Animal/Quail/Quail.jpg","src/Animal/Rabbit/Rabbit.jpg","src/Animal/Shark/Shark.jpg","src/Animal/Tiger/Tiger.jpg","src/Animal/Urial/Urial.jpg","src/Animal/Vulture/Vulture.jpg","src/Animal/Wolf/Wolf.jpg","src/Animal/Xerus/Xerus.jpg","src/Animal/Yalk/Yalk.jpg","src/Animal/Zebra/Zebra.jpg","src/Animal/Zebra/Zebra.jpg","src/Animal/Zebra/Zebra.jpg"};
+
+    public Pages() {
+        init();
+    }
+    public void init() {
+        c = getContentPane();
+        setTitle("Animal Introductory");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setVisible(true);
+
+        prevButton = new JButton("Previous");
+        prevButton.setToolTipText("Previous page");
+        prevButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        prevButton.addActionListener(this);
+        buttonPanel.add(prevButton);
+
+        nextButton = new JButton("Next");
+        nextButton.setToolTipText("Next page");
+        nextButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        nextButton.addActionListener(this);
+        buttonPanel.add(nextButton);
+
+        c.setLayout(new BorderLayout());
+
+        for (int i = 0; i < mainPanel.length; i++) {
+            mainPanel[i] = new JPanel(new GridLayout(2, 2));
+            for (int j = 0; j < 4; j++) {
+                subPanels[subPanelIndex] = new JPanel();
+                subPanels[subPanelIndex].setBackground(Color.white);
+                subPanels[subPanelIndex].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                subPanels[subPanelIndex].setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+                subPanels[subPanelIndex].addMouseListener(this);
+                subPanels[subPanelIndex].addComponentListener(new ComponentAdapter() {
+                    @Override
+                    public void componentResized(ComponentEvent e) {
+                        Component c = e.getComponent();
+                        if (c instanceof JPanel) {
+                            JPanel panel = (JPanel) c;
+                            int panelWidth = panel.getWidth();
+                            int panelHeight = panel.getHeight();
+                            int index = Arrays.asList(subPanels).indexOf(panel);
+                            ImageIcon resizedImg = new ImageIcon(img[index].getImage().getScaledInstance(panelWidth, panelHeight, Image.SCALE_SMOOTH));
+                            imgLabel[index].setIcon(resizedImg);
+                        }
+                    }
+                });
+                img[subPanelIndex] = new ImageIcon(animalName[subPanelIndex]);
+                imgLabel[subPanelIndex] = new JLabel(img[subPanelIndex]);
+                subPanels[subPanelIndex].add(imgLabel[subPanelIndex]);
+                mainPanel[i].add(subPanels[subPanelIndex]);
+                subPanelIndex++;
+            }
+        }
+        c.add(mainPanel[currentPanelIndex], BorderLayout.CENTER);
+        c.add(buttonPanel, BorderLayout.SOUTH);
+        mainPanel[currentPanelIndex].setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == prevButton) {
+            mainPanel[currentPanelIndex].setVisible(false);
+            currentPanelIndex--;
+            if (currentPanelIndex < 0) {
+                currentPanelIndex = mainPanel.length - 1;
+            }
+            mainPanel[currentPanelIndex].setVisible(true);
+            c.add(mainPanel[currentPanelIndex], BorderLayout.CENTER);
+        }
+        else if (e.getSource() == nextButton) {
+            mainPanel[currentPanelIndex].setVisible(false);
+            currentPanelIndex++;
+            if (currentPanelIndex >= mainPanel.length) {
+                currentPanelIndex = 0;
+            }
+            mainPanel[currentPanelIndex].setVisible(true);
+            c.add(mainPanel[currentPanelIndex], BorderLayout.CENTER);
+        }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if(e.getSource()==subPanels[0]) {
+            JOptionPane.showMessageDialog(null, "worked 1");
+        }
+        else if(e.getSource()==subPanels[1]) {
+            JOptionPane.showMessageDialog(null, "worked also 2");
+        }
+        else if(e.getSource()==subPanels[2]) {
+            JOptionPane.showMessageDialog(null, "worked also 3");
+        }
+        else if(e.getSource()==subPanels[3]) {
+            JOptionPane.showMessageDialog(null, "worked also 4");
+        }
+    }
+    public static void main(String[] args) {
+        new Pages();
+    }
+    @Override
+    public void mousePressed(MouseEvent e) {}
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+    @Override
+    public void mouseExited(MouseEvent e) {}
+}
